6b0db9d3e2fde4f12573eacc7826e3104d53b5af
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2022-02-04 18:10:03 -0300

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sys_Vacina.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

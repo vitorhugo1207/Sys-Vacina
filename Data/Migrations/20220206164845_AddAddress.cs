878a33fe40541b0b013c8cc3f43f3b0c5cd84c8c
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2022-02-06 16:44:09 -0300

    Bug in Page Edit Person

---
+++ Data/Migrations/20220206164845_AddAddress.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProgramaEstagio.Data.Migrations
+{
+    public partial class AddAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address_Person_PersonID",
+                table: "Address");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Address_Person_PersonID",
+                table: "Address",
+                column: "PersonID",
+                principalTable: "Person",
+                principalColumn: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address_Person_PersonID",
+                table: "Address");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Address_Person_PersonID",
+                table: "Address",
+                column: "PersonID",
+                principalTable: "Person",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
